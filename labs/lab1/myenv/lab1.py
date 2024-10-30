32bb3a59a1db95aaa53fd731d2ce582881868138
YuraPeretyatko <yuraperetyatko41@gmail.com>
2024-10-30 23:33:18 +0200

    Create lab1.py

---
+++ labs/lab1/myenv/lab1.py
@@ -0,0 +1,18 @@
+
+
+animal = "Пес"
+print(animal) 
+
+is_weekend = True  
+
+is_mammal = True
+has_wings = True 
+
+result = is_mammal and has_wings
+print(result)
+
+x = 1.839
+y = 3.821
+z = 0.349
+res = x**(y+z) + math.sqrt(x+z**y) - 161 * math.tan(x*z)
+print(res)
